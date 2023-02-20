2f9f0d2cc0f492b5dc309540043b5cd7e422240c
Konstantin Fastov <kfastov@gmail.com>
2023-02-20 19:29:50 +0800

    Partially revert c9032db and fully 3edfcd3

--- src/geyser_plugin_postgres.rs
+++ src/geyser_plugin_postgres.rs
@@ -99,9 +99,6 @@ pub enum GeyserPluginPostgresError {
 
     #[error("Error preparing data store schema. Error message: ({msg})")]
     ConfigurationError { msg: String },
-
-    #[error("Replica account V0.0.1 not supported anymore")]
-    ReplicaAccountV001NotSupported,
 }
 
 impl GeyserPlugin for GeyserPluginPostgres {
@@ -231,12 +228,7 @@ impl GeyserPlugin for GeyserPluginPostgres {
 
         let mut measure_all = Measure::start("geyser-plugin-postgres-update-account-main");
         match account {
-            ReplicaAccountInfoVersions::V0_0_1(_) => {
-                return Err(GeyserPluginError::Custom(Box::new(
-                    GeyserPluginPostgresError::ReplicaAccountV001NotSupported,
-                )));
-            }
-            ReplicaAccountInfoVersions::V0_0_2(account) => {
+            ReplicaAccountInfoVersions::V0_0_1(account) => {
                 let mut measure_select =
                     Measure::start("geyser-plugin-postgres-update-account-select");
                 if let Some(accounts_selector) = &self.accounts_selector {
